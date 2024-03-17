ebbbc37379b5e547c70ddda7791295d886ce7313
Nada Hasni <nadasthing@gmail.com>
2024-03-17 10:33:16 +0700

    feat: string datatype

---
+++ go-basic-practice/string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nada")
+	fmt.Println("Nada Hasni")
+	fmt.Println("Nada Hasni Muhammad")
+
+	fmt.Println("Length of my name = ", len("Nada Hasni Muhammad"))
+	fmt.Println("First letter of my name in byte = ", "Nada Hasni Muhammad"[0])
+}
