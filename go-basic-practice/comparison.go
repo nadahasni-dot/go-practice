c43cdb6f92ac1c0da0a169b21f5fecf77dc77dd5
Nada Hasni <nadasthing@gmail.com>
2024-03-17 11:03:15 +0700

    feat: comparison

---
+++ go-basic-practice/comparison.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+
+	fmt.Println("Is a equal b =", a == b)
+	fmt.Println("Is a not equal b =", a != b)
+	fmt.Println("Is a greater than b =", a > b)
+	fmt.Println("Is a less than b =", a < b)
+}
